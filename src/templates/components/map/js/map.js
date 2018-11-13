758c2c8f1dd68e722d21af4add128f2972d3eeee
Hakan Tezcan <hakan.tezcan@clockwork.com.tr>
2018-11-13 12:01:07 +0300

    UIkit sıfırlandı

--- src/templates/components/map/js/map.js
+++ src/templates/components/map/js/map.js
@@ -1,12 +1,12 @@
 export function initMap(mapContainer) {
 
-    let uluru = {lat: 35.3795884, lng: 34.086817};
+    let uluru = {lat: -25.363, lng: 131.044};
     let map = new google.maps.Map(mapContainer, {
-        zoom: 13,
+        zoom: 4,
         center: uluru
     });
 
-    let contentString = '<div class="content"><h4>' + "Noah's Ark Deluxe Hotel & Spa" + '</h4><p>İskele Bafra Turizm Bölgesi, Vokolida</p></div>';
+    let contentString = '<div class="content"><h4>Address</h4><p>Explanation</p></div>';
 
     let infowindow = new google.maps.InfoWindow({
         content: contentString
@@ -16,10 +16,10 @@ export function initMap(mapContainer) {
     let marker = new google.maps.Marker({
         position: uluru,
         map: map,
-        title: 'Uluru (Ayers Rock)'
+        title: 'Uluru (Ayers Rock)',
+        icon: image
     });
     marker.addListener('click', function () {
         infowindow.open(map, marker);
     });
-
 };
